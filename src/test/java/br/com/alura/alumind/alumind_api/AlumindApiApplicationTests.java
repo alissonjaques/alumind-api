564a576f25d66d4d7829ac0b8b4a8b3d2d1e31a5
Alisson Jaques <alissonjaquesrmq@gmail.com>
2024-11-11 13:52:08 -0300

    feat: estrutura inicial do projeto

---
+++ src/test/java/br/com/alura/alumind/alumind_api/AlumindApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.alumind.alumind_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumindApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
